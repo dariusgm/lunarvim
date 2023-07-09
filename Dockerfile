18b8f695248cff0e174e108e7f92721184c03dd2
Darius Murawski <darius@murawski.blog>
2023-07-09 21:13:39 +0200

    Correctly Call masoninstal and Treesitter for lsp, formatter and linters.

--- Dockerfile
+++ Dockerfile
@@ -16,21 +16,55 @@ COPY --from=aws /usr/local/bin /usr/local/bin
 COPY --from=node /usr/local/lib/node_modules /usr/local/lib/node_modules
 COPY --from=node /usr/local/bin /usr/local/bin
 COPY --from=rust /usr/local/cargo/bin /usr/local/cargo/bin
-ENV PATH="/root/.cargo/bin:$PATH"
+ENV PATH="/root/.cargo/bin:/usr/local/cargo/bin/:$PATH"
 
 # neovim and lunarvim
 ARG LV_BRANCH=release-1.3/neovim-0.9
 RUN  curl -LSs https://raw.githubusercontent.com/lunarvim/lunarvim/${LV_BRANCH}/utils/installer/install-neovim-from-release | bash && \
      curl -LSs https://raw.githubusercontent.com/lunarvim/lunarvim/${LV_BRANCH}/utils/installer/install.sh | bash -s -- --no-install-dependencies
 ENV PATH="/root/.local/bin:$PATH"
-## core update
-RUN lvim +LvimUpdate +LvimSyncCorePlugins +qall
-
+## core update to trigger first boot
+RUN lvim +LvimUpdate +qall
 COPY config.lua /root/.config/lvim/config.lua
-RUN lvim --headless +'checkhealth lazy' +qall
+RUN lvim --headless +LvimReload +qall
 RUN lvim --headless +'Lazy sync' +qall
-# RUN lvim +'MasonInstall black' +qall
+# LSP
+RUN lvim --headless +'MasonInstall ansible-language-server' +qall
+RUN lvim --headless +'MasonInstall cmake-language-server' +qall
+RUN lvim --headless +'MasonInstall css-lsp' +qall
+RUN lvim --headless +'MasonInstall docker-compose-language-service' +qall
+RUN lvim --headless +'MasonInstall dockerfile-language-server' +qall
+RUN lvim --headless +'MasonInstall html-lsp' +qall
+RUN lvim --headless +'MasonInstall python-lsp-server' +qall
+RUN lvim --headless +'MasonInstall lua-language-server' +qall
 
-
+# toml
+RUN lvim --headless +'MasonInstall taplo' +qall
+RUN lvim --headless +'MasonInstall terraform-ls' +qall
+RUN lvim --headless +'MasonInstall tflint' +qall
+# DAP
+RUN lvim --headless +'MasonInstall bash-debug-adapter' +qall
+# DAP Python
+RUN lvim --headless +'MasonInstall debugpy' +qall
+# Linter
+RUN lvim --headless +'MasonInstall tflint yamllint markdownlint jsonlint flake8 curlylint ansible-lint' +qall
+# Formatter
+# RUN lvim --headless +'MasonInstall htmlbeautifier' +qall
+# rust
+RUN lvim --headless +'MasonInstall rustfmt' +qall
+# yaml / yml
+RUN lvim --headless +'MasonInstall yamlfmt' +qall
+# jq
+RUN lvim --headless +'MasonInstall jq' +qall
+# python
+RUN lvim --headless +'MasonInstall black' +qall
+# Treesitter
+RUN lvim --headless +'TSInstallSync lua' +qall
+RUN lvim --headless +'TSInstallSync query' +qall
+RUN lvim --headless +'TSInstallSync markdown_inline' +qall
+RUN lvim --headless +'TSInstallSync comment' +qall
+RUN lvim --headless +'TSInstallSync regex' +qall
+RUN lvim --headless +'TSInstallSync vim' +qall
+RUN lvim --headless +'TSInstallSync vimdoc' +qall
 ## install font on your client
 # curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/UbuntuMono.zip -o UbuntuMono.zip
--- config.lua
+++ config.lua
@@ -1,79 +1,41 @@
-vim.opt.timeoutlen = 100
 lvim.plugins = {
-  { "nvim-treesitter/nvim-treesitter-textobjects"},
-  { "nvim-treesitter/playground" },
-  {
-      "jay-babu/mason-nvim-dap.nvim",
-      opts = {
-        ensure_installed = { "python" }
-      }
+    {
+        'wfxr/minimap.vim',
+        build = "cargo install --locked code-minimap",
+        -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
+        config = function()
+            vim.cmd("let g:minimap_width = 10")
+            vim.cmd("let g:minimap_auto_start = 1")
+            vim.cmd("let g:minimap_auto_start_win_enter = 1")
+        end,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        config = function()
+            require("nvim-ts-autotag").setup()
+        end,
+    },
+    {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        event = "BufRead",
+    },
+    {
+        "mrjones2014/nvim-ts-rainbow",
+    },
+    {
+        "metakirby5/codi.vim",
+        cmd = "Codi",
+    },
+    -- You must install glow globally
+    -- https://github.com/charmbracelet/glow
+    -- yay -S glow
+    {
+        "npxbr/glow.nvim",
+        ft = { "markdown" }
+        -- build = "yay -S glow"
     },
 }
--- setup treesitter
-require('nvim-treesitter.configs').setup {
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-  },
-}
 
--- DAP
-
--- dap
-local dap = require('dap')
-dap.adapters.python = {
-  type = 'executable';
-  command = '/usr/local/bin/python';
-  args = { '-m', 'debugpy.adapter' };
-}
-
-dap.configurations.python = {
-  {
-    type = 'python';
-    request = 'launch';
-    name = "Launch file";
-    program = "${file}";
-    pythonPath = function()
-      local cwd = vim.fn.getcwd()
-      if vim.fn.glob(cwd .. '/venv/bin/python') ~= '' then
-        return cwd .. '/venv/bin/python'
-      elseif vim.fn.glob(cwd .. '/.venv/bin/python') ~= '' then
-        return cwd .. '/.venv/bin/python'
-      else
-        return '/usr/local/bin/python'
-      end
-    end;
-  },
-}
-
--- misc options
-lvim.log.level = "info"
 lvim.leader = "space"
-lvim.format_on_save = true
-lvim.lint_on_save = true
-lvim.builtin.gitsigns.active = true
-lvim.builtin.breadcrumbs.active = true
--- highlight/match brackets
-lvim.builtin.treesitter.matchup.enable = true
-
--- automatically update html tags when editing
-lvim.builtin.treesitter.autotag.enable = true
-
--- enable incremental selection with <CR> and <tab>/<s-tab>
-lvim.builtin.treesitter.incremental_selection = {
-	module_path = "nvim-treesitter.incremental_selection",
-	enable = true,
-	keymaps = {
-		init_selection = "<CR>",
-		node_incremental = "<CR>",
-		node_decremental = "<TAB>",
-		scope_incremental = "<S-TAB>",
-	},
-	is_supported = function()
-		return true
-	end,
-}
-
-
-lvim.builtin.project.active = false
-
+lvim.builtin.treesitter.rainbow.enable = true
+lvim.lsp.automatic_servers_installation = false
