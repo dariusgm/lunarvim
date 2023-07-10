835d6a6526c12c4015ac0baad02a4ebeed648044
Darius Murawski <dariusmurawski@hotmail.com>
2023-07-10 22:08:22 +0200

    Add lazygi tests

---
+++ tests/lazygit.py
@@ -0,0 +1,15 @@
+import subprocess
+import re
+def test_lazygit():
+
+    # commit=5149b24ab3dfad3860e2300519c7c583dcc8c9ff, build date=2023-05-03T08:00:22Z, build source=binaryRelease, version=0.38.2, os=linux, arch=amd64, git version=2.34.1
+    output = str(subprocess.check_output('lazygit --version', shell=True), encoding='utf8')
+    search = re.search(r'version=\d\.\d\d\.\d', output)
+    if search:
+        major, minor, patch = search.group(0).split("=")[-1].split(".")
+        assert(int(major) >= 0)
+        assert(int(minor) >= 38)
+        assert(int(patch) >= 0)
+    else:
+        raise AssertionError("expected match")
+
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,4 +1,3 @@
-import pytest
 import subprocess
 
 def test_git():
