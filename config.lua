bf130fcbae27efe69060f1de88e3dfc273e658da
Daniel Krieg <dk.daniel.krieg@gmail.com>
2023-08-10 15:29:47 +0000

    Use relative line numbers as default

--- config.lua
+++ config.lua
@@ -131,3 +131,6 @@ lvim.format_on_save                     = true
 lvim.leader                             = "space"
 lvim.builtin.treesitter.rainbow.enable  = true
 lvim.lsp.automatic_servers_installation = false
+
+-- relative line numbers as default
+vim.wo.relativenumber                   = true
