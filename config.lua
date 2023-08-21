ddd6e150c0a9db28afc71e93c3d1bb8ff97d7d36
Darius Murawski <dariusmurawski@hotmail.com>
2023-08-21 14:29:04 +0200

    Set relativ Line number as default

--- config.lua
+++ config.lua
@@ -131,3 +131,5 @@ lvim.format_on_save                     = true
 lvim.leader                             = "space"
 lvim.builtin.treesitter.rainbow.enable  = true
 lvim.lsp.automatic_servers_installation = false
+vim.opt.relativenumber                  = true
+vim.opt.number                          = true
